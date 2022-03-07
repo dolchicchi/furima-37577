46e934b190003a83ceeeb73b57c1153944640b7a
dolchicchi <m.m.m.m.mor0o@gmail.com>
2022-03-07 13:20:51 +0900

    マイグレーションファイル修正

--- db/migrate/20220307003528_devise_create_users.rb
+++ db/migrate/20220307003528_devise_create_users.rb
@@ -4,14 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :nickname,           null: false, default: ""
+      t.string :nickname,           null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :last_name,          null: false, default: ""
-      t.string :first_name,         null: false, default: ""
-      t.string :kana_last_name,     null: false, default: ""
-      t.string :kana_first_name,    null: false, default: ""
-      t.string :birthday,           null: false, default: ""
+      t.string :last_name,          null: false
+      t.string :first_name,         null: false
+      t.string :kana_last_name,     null: false
+      t.string :kana_first_name,    null: false
+      t.date   :birthday,           null: false
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,14 @@
 ActiveRecord::Schema.define(version: 2022_03_07_003528) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", default: "", null: false
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "last_name", default: "", null: false
-    t.string "first_name", default: "", null: false
-    t.string "kana_last_name", default: "", null: false
-    t.string "kana_first_name", default: "", null: false
-    t.string "birthday", default: "", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "kana_last_name", null: false
+    t.string "kana_first_name", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
