0c6e3c6f2e07454875147955069e9e4d12bb0e67
dolchicchi <m.m.m.m.mor0o@gmail.com>
2022-03-08 17:04:17 +0900

    itemモデルバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  # アソシエーション
   belongs_to :user
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
@@ -7,4 +8,19 @@ class Item < ApplicationRecord
   belongs_to :postage
   belongs_to :region
   belongs_to :days_required
+
+  # バリデーション
+  with_options presence: true do
+    validates :name
+    validates :explanation
+    validates :price
+    validates :image
+  end
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :status_id
+    validates :postage_id
+    validates :region_id
+    validates :days_required_id
+  end
 end
