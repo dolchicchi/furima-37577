bfef3d4fac5d4b18eed20bec55678a1e48534685
dolchicchi <m.m.m.m.mor0o@gmail.com>
2022-03-08 15:59:12 +0900

    商品登録機能(画像除く)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,5 +7,19 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(params_item)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def params_item
+    params.require(:item).permit(
+      :name, :explanation, :category_id, :status_id,
+      :postage_id, :region_id, :days_required_id, :price
+    ).merge(user_id: current_user.id)
   end
 end
