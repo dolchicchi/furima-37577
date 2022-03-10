0dd7ed4ddce73c4ae8dd0e1da7e01b74ae142954
dolchicchi <m.m.m.m.mor0o@gmail.com>
2022-03-10 21:40:32 +0900

    クレジット決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,5 +2,34 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @order = Order.new
+  end
+
+  def new
+    
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+  def order_params
+    order_params = {user_id: current_user.id, item_id: params[:item_id], token: params[:token]}
+  end
+  def pay_item
+    Payjp.api_key = "sk_test_65764451d9433f1545387000"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: Item.find(params[:item_id]).price,  # 商品の値段
+      card: params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 end
